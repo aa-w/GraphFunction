c849cd08c724e875f0743083b8539964ef84d62f
Alex Woodley <eartlsaki@gmail.com>
2019-12-16 00:33:55 +0000

    Full graphing

--- GraphFunction/GraphFunction.ino
+++ GraphFunction/GraphFunction.ino
@@ -12,29 +12,33 @@
 //#define WAVEFORMCHARTY (0.1 * SCREENHEIGHT)
 
 #define WAVEFORMCHARTX 0
-#define WAVEFORMCHARTY 35
+#define WAVEFORMCHARTY 10
 
-#define WAVEFORMLENGTH 10
+#define WAVEFORMLENGTH 20
 
 int16_t TouchedTag;
 int16_t WaveFormWidth = 300;
 int16_t WaveFormHeight = 250;
 int16_t dur = 0;
-int pincount = 0;
+
 char CharOutput [12];
 
-int ShiftRegister [WAVEFORMLENGTH];
-long ShiftRegisterTimer = millis() + 1000;
+int LoopVal = 0;
+int16_t ShiftRegister [WAVEFORMLENGTH];
+unsigned long ShiftRegisterTimer;
 
-void setup() {
+void setup()
+{
 
   CleO.begin();
   CleO.DisplayRotate(2, 0);
-  CleO.SetBackgroundcolor(BLACK);
+  CleO.SetBackgroundcolor(WHITE);
   pinMode(5, INPUT_PULLUP);
 
-  Serial.begin(9600);
-  WaveformSetup();
+  Serial.begin(19200);
+
+  CleO.PointColor(BLUE);
+  CleO.PointRadius(3);
 }
 
 void loop()
@@ -43,7 +47,7 @@ void loop()
   //Update touch
   int16_t Tempx, Tempy, Tempdur, TempTouchedTag;
   CleO.TouchCoordinates(Tempx, Tempy, Tempdur, TempTouchedTag);
-  
+
   if (Tempdur == 1)
   {
     WaveFormWidth = Tempx;
@@ -51,21 +55,62 @@ void loop()
     dur = Tempdur;
     TouchedTag = TempTouchedTag;
   }
-  
+
   CleO.Start();
-  
+
+  //unsigned long CurrentSecond = ((millis() / 1000U) % 10);
+
+
+
+  //Serial.println(millis());
+
+
   BuildGraph();
-  PlotValue(1,1);
-  PlotValue(2,2);
-  PlotValue(3,3);
-  PlotValue(4,4);
-  PlotValue(5,5);
-  PlotValue(6,6);
-  PlotValue(7,5);
-  PlotValue(8,8);
+
+  
+    if (ShiftRegisterTimer < millis())
+    {
+    UpdateShiftRegister();
+
+    ShiftRegisterTimer = millis() + 50;
+    }
+  
+
+
+  /*
+  if (LoopVal != 10)
+  {
+    
+    LoopVal++;
+  }
+  else
+  {
+    LoopVal = 0;
+  }
+  */
+  
+  //UpdateShiftRegister(LoopVal);
+
+  PlotShiftRegister();
+
+  //AnimationLoop();
   CleO.Show();
 
+
 }
+
+/*
+  void AnimationLoop() //Displays the frame refresh rate
+  {
+  CleO.LineColor(RED);
+
+  //unsigned long millisx = millis();
+  double CurrentSecond = ((millis() / 1000U) % 10); //Gets the seconds in millis
+  double CurrentMiliSecond = ((millis() / 100U) % 10);
+
+  CleO.Line((0 * SCREENWIDTH), (0.985 * SCREENHEIGHT), (((CurrentSecond / 10) + (CurrentMiliSecond / 100)) * SCREENWIDTH), (0.985 * SCREENHEIGHT));
+  }
+*/
 
 void BuildGraph()
 {
@@ -90,37 +135,37 @@ void BuildGraph()
   int YPositionCoords = 0 - YPositionCoords;
 
   //X position Placement
-  for (int PositionPlacement = 0; PositionPlacement != WAVEFORMLENGTH; PositionPlacement++)
+  for (int PositionPlacement = 0; PositionPlacement != (WAVEFORMLENGTH + 1); PositionPlacement++)
   {
     itoa(PositionPlacement, Value, 10);
     if (ScaleXPosition > 8)
     {
-      CleO.StringExt(FONT_SANS_3, ((WAVEFORMCHARTX + 25) + XPositionCoords), (WaveFormHeight + 20), BLACK, MM, 0, 0, Value);
+      CleO.StringExt(FONT_SANS_3, ((WAVEFORMCHARTX + 25) + XPositionCoords), (WaveFormHeight - 15), BLACK, MM, 0, 0, Value);
     }
     else
     {
       if (PositionPlacement == 0 || PositionPlacement == (WAVEFORMLENGTH - 1))
       {
-        CleO.StringExt(FONT_SANS_3, ((WAVEFORMCHARTX + 25) + XPositionCoords), (WaveFormHeight + 20), BLACK, MM, 0, 0, Value);
+        CleO.StringExt(FONT_SANS_3, ((WAVEFORMCHARTX + 25) + XPositionCoords), (WaveFormHeight - 15), BLACK, MM, 0, 0, Value);
       }
     }
     XPositionCoords = ScaleXPosition + XPositionCoords;
   }
 
   //Y position Placement
-  for (int PositionPlacement = 0; PositionPlacement != WAVEFORMLENGTH; PositionPlacement++)
+  for (int PositionPlacement = 0; PositionPlacement != (WAVEFORMLENGTH + 1); PositionPlacement++)
   {
     itoa(PositionPlacement, Value, 10);
 
     if (ScaleYPosition > 8)
     {
-      CleO.StringExt(FONT_SANS_3, (WAVEFORMCHARTX + 10), (WaveFormHeight - YPositionCoords), BLACK, MM, 0, 0, Value);
+      CleO.StringExt(FONT_SANS_3, (WAVEFORMCHARTX + 20), (WaveFormHeight - YPositionCoords), BLACK, MM, 0, 0, Value);
     }
     else
     {
       if (PositionPlacement == 0 || PositionPlacement == (WAVEFORMLENGTH - 1))
       {
-        CleO.StringExt(FONT_SANS_3, (WAVEFORMCHARTX + 10), (WaveFormHeight - YPositionCoords), BLACK, MM, 0, 0, Value);
+        CleO.StringExt(FONT_SANS_3, (WAVEFORMCHARTX + 20), (WaveFormHeight - YPositionCoords), BLACK, MM, 0, 0, Value);
       }
     }
     YPositionCoords = ScaleYPosition + YPositionCoords;
@@ -133,45 +178,49 @@ int GraphRegionX()
   return WaveFormWidth - 25;
 }
 
-int GraphRegionY() 
+int GraphRegionY()
 {
   return WaveFormHeight - 25;
 }
 
-void WaveformSetup() //Called once at setup used to set the parameters of the graph
+void PlotShiftRegister()
 {
-
-}
-
-void SetWaveformValue (int Value)
-{
-  
+  CleO.PointColor(RED);
+  CleO.PointRadius(3);
+  for (byte index = 0; index < WAVEFORMLENGTH; index++)
+  {
+    //CleO.Point(((WaveFormWidth / WAVEFORMLENGTH) * index), ((WaveFormHeight / WAVEFORMLENGTH) * ShiftRegister[index]));
+    if (index != 0 && index != WAVEFORMLENGTH)
+    {
+      CleO.LineColor(BLUE);
+      CleO.Line((((WaveFormWidth / WAVEFORMLENGTH) * (index - 1)) + 25), (((WaveFormHeight / WAVEFORMLENGTH) * ShiftRegister[index - 1]) + 25), (((WaveFormWidth / WAVEFORMLENGTH) * index) + 25), (((WaveFormHeight / WAVEFORMLENGTH) * ShiftRegister[index]) + 25));
+    }
+  }
 }
 
 void PlotValue(int x, int y)
 {
-  CleO.PointColor(BLACK);
+  CleO.PointColor(RED);
   CleO.PointRadius(3);
-  CleO.Point(((WaveFormWidth / WAVEFORMLENGTH) * x),((WaveFormHeight / WAVEFORMLENGTH) * y));
-  //Serial.println(x);
-  //Serial.println(y);
-  //Serial.println((GraphRegionX() / WAVEFORMLENGTH) * x);
-  //Serial.println((GraphRegionY() / WAVEFORMLENGTH) * y);
+
 }
 
 void PlotGrid()
 {
   //CleO.Line((0 * SCREENWIDTH), (0.985 * SCREENHEIGHT), (((CurrentSecond / 10) + (CurrentMiliSecond / 100)) * SCREENWIDTH), (0.985 * SCREENHEIGHT));
 }
-  
+
 void UpdateShiftRegister()
 {
-  if(ShiftRegisterTimer > millis)
+
+  //double CurrentSecond = ((millis() / 1000U) % 10); //Gets the seconds in millis
+  double CurrentMiliSecond = ((millis() / 100U) % 10);
+  int NewValue = (int) CurrentMiliSecond;
+  for (int index = WAVEFORMLENGTH; index != 0; index--)
   {
-  for(byte index = 0; index != WAVEFORMLENGTH; index++)
-  {
-  ShiftRegister[index + 1] = ShiftRegister [index];
+    ShiftRegister[index] = ShiftRegister[index - 1];
   }
-  }
-  
+
+  ShiftRegister[0] = NewValue;
+  Serial.println(NewValue);
 }
